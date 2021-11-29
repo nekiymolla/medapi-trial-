65cd166469fb25dac00e4184a269898a6802c4a1
Ivan <Ivan@DESKTOP-C7DQIQC>
2021-11-29 09:50:23 +0300

    Добавьте файлы проекта.

---
+++ medapi(trial)/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace medapi_trial_.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SeName { get; set; }
+        public string ThName { get; set; }
+        public int Age { get; set; }
+        public float Сholesterol { get; set; }
+        public float Pressure { get; set; }
+        public string Geolocation { get; set; }
+        
+    }
+}
---
+++ medapi(trial)/UsersController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using medapi_trial_.Models;
+
+namespace medapi_trial_
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        UsersContext db;
+        public UsersController(UsersContext context)
+        {
+            db = context;
+            if (!db.Users.Any())
+            {
+                db.Users.Add(new User { Name = "Ilnar", SeName = "Bajteev", ThName="Radikovich", Age = 18, Сholesterol=4, Pressure=220, Geolocation="Orenburg" });
+                db.Users.Add(new User { Name = "Michael", SeName = "Dzuba", ThName = "Vyacheslavovich", Age = 18, Сholesterol = 4, Pressure = 120, Geolocation = "Orenburg" });
+                db.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await db.Users.ToListAsync();
+        }
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public async Task <ActionResult<User>> Get(int id)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(user);
+        }
+        // POST api/users
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+        // PUT api/users/
+        [HttpPut]
+        public async Task <ActionResult<User>> Put(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            if (!db.Users.Any(x => x.Id == user.Id))
+            {
+                return NotFound();
+            }
+            db.Update(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> Delete(int id)
+        {
+            User user = db.Users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            db.Users.Remove(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+    }
+}
+//44346
+
